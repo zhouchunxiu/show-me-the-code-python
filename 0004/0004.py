57046d68c7825f945bc85f68fef4888b324446c1
zcx <51038269+zhouchunxiu@users.noreply.github.com>
2020-09-09 20:43:22 +0800

    finish 0004

---
+++ 0004/0004.py
@@ -0,0 +1,28 @@
+# 任一个英文的纯文本文件，统计其中的单词出现的个数
+import os
+import re
+
+
+def count_words(file):
+    with open(file, "r") as fd:
+        data = fd.read().splitlines()
+    res = {}
+    for line in data:
+        words = re.sub(r"[.?!,""/]", " ", line)   # 英文中可能出现的，要替换的标点符号
+        for word in words.split(" "):
+            key = res.get(word, None)
+            if key is None:
+                res[word] = 1
+            else:
+                res[word] += 1
+
+    print_res(res)
+
+
+def print_res(dict):
+    for key, value in dict.items():
+        print("word:", key, "\tcount:", value)
+
+
+if __name__ == "__main__":
+    count_words(os.path.join(os.getcwd(), "file"))
